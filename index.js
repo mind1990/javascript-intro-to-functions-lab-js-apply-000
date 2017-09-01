1f00ce9abbb3c0bc86154d57c402c7594fe448f2
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-01 16:28:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  var lowercase = "hello!"
+
   if (string.toLowerCase() === lowercase)
   console.log("I can't hear you!")
 }
