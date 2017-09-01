b96441af0bb3ad78debc3249e3c6bb877d3163b7
Tirapat Numsinvichietchai <mind1990@gmail.com>
2017-09-01 16:20:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandsma(string) {
-  if string === "I love you, Grandma."
+  if (string === "I love you, Grandma.")
   return "I love you too."
 }
